eda52a731b34becc12ee95449cf82913d026bd8f
Yuri <wall.mart.boss@proton.me>
2024-08-01 22:37:11 +0300

    initial2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,14 @@
-import { useEffect, useState } from 'react';
 import Description from "./Description/Description";
-import Options from "./Options/Options";
-import Feedback from "./Feedback/Feedback";
-import Notification from "./Notification/Notification";
+
 
 
 const App = () => {
-   const [feedbacks, setFeedbacks] = useState(() => {
-    const savedData = JSON.parse(window.localStorage.getItem('fbks'));
-    if (savedData) {
-      return savedData;
-    }
-    return {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-   });
-
-  const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
-
-const updateFeedback = feedbackType => {
- setFeedbacks(prev => ({ ...prev, [feedbackType]: prev[feedbackType] + 1 }));
-  };
-  useEffect(() => {
-    window.localStorage.setItem('fbks', JSON.stringify(feedbacks));
-  }, [feedbacks]);
-
-  const feedbackReset = () => {
-            setFeedbacks({
-                good: 0,
-                neutral: 0,
-                bad: 0,
-            });
-  };
-  
-  const perc_positive = Math.round((feedbacks.good / totalFeedback) * 100);
+   
 
   return (
     <>
       <Description />
-      <Options
-        totalFeedback={totalFeedback}
-        updateFeedback={updateFeedback}
-        feedbackReset={feedbackReset} />
-      { totalFeedback > 0 && 
-      <Feedback
-        feedbacks={feedbacks}
-        totalFeedback={totalFeedback}
-        perc_positive={perc_positive} /> }
-      { !totalFeedback && <Notification /> } 
+
     </>
   );
 };
--- src/components/Notification/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./Notification.module.css" 
-
-const Notification = () => {
-    return (
-        <p className={s.notification_text}>No feedback yet</p>
-    );
-};
-
-export default Notification;
